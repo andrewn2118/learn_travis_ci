b800ed69bb4e81b7c382f65ac48604067e6efe8e
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2016-07-29 14:10:14 -0700

    adding second test

--- test/test.js
+++ test/test.js
@@ -5,4 +5,8 @@ describe('add function', function() {
   it('should return 2', function() {
     expect(app.add(1, 1)).to.equal(2);
   });
+
+  it('should return 3', function() {
+    expect(app.add(1, 1)).to.equal(3);
+  });
 });

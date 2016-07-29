3d6da47a0daf2acc175d5c649fe208c2219cb81a
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2016-07-29 14:06:53 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var app = require('../app.js');
+var expect = require('chai').expect;
+
+describe('add function', function() {
+  it('should return 2', function() {
+    expect(app.add(1, 1)).to.equal(2);
+  });
+});
